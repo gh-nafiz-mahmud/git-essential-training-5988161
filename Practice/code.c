9f94ea83d71df8611abdffafd1c4e5ca731bd1d8
Nafiz <work.nafiz@gmail.com>
2025-07-02 17:43:47 +0600

    add some basic code in 2ndbranch

--- Practice/code.c
+++ Practice/code.c
@@ -6,6 +6,14 @@ int main()
   printf("Here new line\n");
 
   // single line comment
-  
+
+  // writing this code on 2ndbranch
+
+  print("I am the bosssss");
+  int a, b;
+  scanf("%d %d", &a, &b);
+  int mul = a * b;
+  printf("%d", mul);
+
   return 0;
 }
